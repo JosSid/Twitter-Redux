b6cfbee76bb660bc4e9abc9a0a2e6c04ebab362c
Josep Valls <alfredvays@gmail.com>
2022-12-21 12:57:46 +0100

    High order reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import thunk from 'redux-thunk';
-import * as auth from "../components/auth/service";
-import * as tweets from "../components/tweets/service";
+import * as auth from '../components/auth/service';
+import * as tweets from '../components/tweets/service';
 //import  {auth, tweets } from './reducers';
 
 import * as reducers from './reducers';
@@ -16,37 +16,63 @@ const reducer = combineReducers(reducers);
     };
     return next(action);
 }; */
-
-const logger = store => next => action => {
-    console.group(action.type);
-    console.info('dispatching', action, store.getState());
-    const result = next(action);
-    console.log('next state', store.getState());
-    console.groupEnd();
-    return result;
-};
-
-const failuredRedirections = (router, redirections) => store => next => action => {
-    const result = next(action);
-
-    if(action.error){
-        
-        const redirection = redirections[action.payload.status]
-        if(redirection){
-            router.navigate(redirection);
-        }
+const HISTORY_BACK = 'HISTORY_BACK';
+const historyHighOrderReducer = reducer => {
+  return (state, action) => {
+    const { history = [], ...rootState } = state;
+    if (action.type === HISTORY_BACK) {
+      const newHistory = history.slice(0, history.length - 1);
+      return {
+        ...newHistory[newHistory.length - 1].state,
+        history: newHistory,
+      };
     }
-    
-    return result;
-}
 
-
-export default function configureStore (preloadedState, {router}) {
-    const middlewares = [thunk.withExtraArgument({api: {auth,tweets}, router}), failuredRedirections(router, {
-        401: '/login',
-        404: '/404'
-    }), logger];
-    const store = createStore(reducer, preloadedState,   composeWithDevTools(applyMiddleware(...middlewares)));
-
-    return store;
+    const newState = reducer(rootState, action);
+    return {
+      ...newState,
+      history: [...history, { action, state: newState }],
+    };
+  };
 };
+
+const logger = (store) => (next) => (action) => {
+  console.group(action.type);
+  console.info('dispatching', action, store.getState());
+  const result = next(action);
+  console.log('next state', store.getState());
+  console.groupEnd();
+  return result;
+};
+
+const failuredRedirections =
+  (router, redirections) => (store) => (next) => (action) => {
+    const result = next(action);
+
+    if (action.error) {
+      const redirection = redirections[action.payload.status];
+      if (redirection) {
+        router.navigate(redirection);
+      }
+    }
+
+    return result;
+  };
+
+export default function configureStore(preloadedState, { router }) {
+  const middlewares = [
+    thunk.withExtraArgument({ api: { auth, tweets }, router }),
+    failuredRedirections(router, {
+      401: '/login',
+      404: '/404',
+    }),
+    logger,
+  ];
+  const store = createStore(
+    historyHighOrderReducer(reducer),
+    preloadedState,
+    composeWithDevTools(applyMiddleware(...middlewares))
+  );
+
+  return store;
+}
